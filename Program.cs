6b11de6b279f252e6f890dd1dca543481e2b336a
LAPTOP-B91IM17H <1940484097@qq.com>
2023-07-31 11:13:16 +0800

    提交

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Learn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
